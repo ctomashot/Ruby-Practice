627223b2b558f20430a390a01c2dfc16060e30f8
ctomashot <72528439+ctomashot@users.noreply.github.com>
2021-05-02 12:46:40 -0500

    Update questions.rb

--- questions.rb
+++ questions.rb
@@ -81,6 +81,11 @@ puts fibonacci( 5 )
 # output the number in words 
 # ie: 384765 output three hundred eighty four thousand seven hundred sixty five
 
+num2words1 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
+            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
+            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
+            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
+num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
 
 def number(Number):
     if 0 <= Number <= 19:
