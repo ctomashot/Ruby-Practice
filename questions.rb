37f05a2d41317b3df55de20174d7954bb8358ce6
ctomashot <72528439+ctomashot@users.noreply.github.com>
2021-10-12 14:45:12 -0500

    Update questions.rb

--- questions.rb
+++ questions.rb
@@ -1,7 +1,6 @@
 # 1. given a sentence output this in reverse order 
 # input: "this is a test string" 
 # output: "string test a is this"
- 
 string = "this is a test string"
 def reverse(string)
   a = string.split(" ")
